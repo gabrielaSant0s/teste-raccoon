7e551e823b99fd1748c9c1d36b612096ac42a2df
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-10 16:41:11 -0300

    redirecionamento da pag de login para produtos

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,36 +1,46 @@
 import React, {createContext, useState} from "react";
 
+import { useNavigate } from "react-router-dom"
+
 export const AuthContext = createContext()
 
 export const AuthProvider = ({children}) =>{
+    const navigate = useNavigate()
+
     const [user, setUser] = useState(null)
 
-  const login = (email, password) => {
-    console.log("login", {email, password});
-    setUser(
-      fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+    const login = (email, password) => {
+        console.log("login", {email, password});
+
+        if (email === 'kminchelle@qq.com' && password ==='0lelplR'){
+            setUser(
+                fetch('https://dummyjson.com/auth/login', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
     
-        username: 'kminchelle',
-        password: '0lelplR',
+                    username: 'kminchelle',
+                    password: '0lelplR',
     
-        })
-      })
-    .then(res => res.json())
-    .then(console.log)
+                })
+            })
+            .then(res => res.json())
+            .then(console.log)
+            )
+            navigate("/produtos")
+        }   
+    }
+
+    const logout = () => {
+        console.log("logout");
+        setUser(null)
+        navigate("/login")
+    }
+
+    return(
+        <AuthContext.Provider
+        value={{ authenticated: !!user, user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
     )
-  }
-
-  const logout = () => {
-    console.log("logout");
-  }
-
-  return(
-    <AuthContext.Provider
-    value={{ authenticated: !!user, user, login, logout}}>
-        {children}
-    </AuthContext.Provider>
-  )
 }
