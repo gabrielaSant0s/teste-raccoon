5a40008a1a60f41eee6105e0cd4721ffc0df8004
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-10 18:12:59 -0300

    botao logout

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -48,6 +48,7 @@ export const AuthProvider = ({children}) =>{
 
     const logout = () => {
         console.log("logout");
+        localStorage.removeItem("user")
         setUser(null)
         navigate("/login")
     }
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useContext} from "react";
 import MenuBar from "../../components/atoms/menu-bar/index"
 import ProductCard from "../../components/atoms/product-card";
 
+import {AuthContex, AuthContext} from "../../contexts/auth"
 
 const Product = () => {
+    const {logout} = useContext(AuthContext)
+
+    const handleLogout = () => {
+        logout()
+    }
     return(
         <div>
             <MenuBar/>
             <h1>Página Produto</h1>
             <ProductCard/>
+            <button onClick={handleLogout}>logout</button>
         </div>
     )
 }
