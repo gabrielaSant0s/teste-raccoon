71d9a925d5efd20777c7e9603915763e6fe829c1
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-10 15:28:51 -0300

    criando interação com o submit no form de login

--- src/components/atoms/user-login/index.js
+++ src/components/atoms/user-login/index.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css"
 
 const UserLogin = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log("submit", {email, password});
+    }
     return (
         <div className="loginContainerComponent">
             <h1 className="title">Acessar o sistema</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="containerLabels">
                     <label className="titleLabel">E-mail</label>
                     <input 
                         className="input"
                         type="email" 
-                        id="email"
-                        pattern=".+@globex\.com"
-                        size="30" 
+                        id="email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required></input>
                 </div>
                 <div className="containerLabels">
@@ -23,7 +30,9 @@ const UserLogin = () => {
                         type="password" 
                         id="pass" 
                         name="password"
-                        minlength="8" 
+                        minlength="8"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} 
                         required></input>
                 </div>
                 <button className="buttonLoginUser" type="submit" value="Entrar">Entrar</button>
