6eddfe775fea7b13c1c05e518d6dd77cf4bec4d6
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-11 13:46:27 -0300

    pagination da tela produtos

--- src/components/molecules/products-div/index.js
+++ src/components/molecules/products-div/index.js
@@ -6,6 +6,8 @@ import ProductCard from "../../atoms/product-card";
 
 const ProductsDiv = () => {
     const [dataProducts,setDataProducts] = useState([]);
+    const [itensPerPage, setItensPerPage] = useState(6)
+    const [currentPage, setCurrentPage] = useState(0)
 
     useEffect (()=>{
         apiProducts.get("products").then(({data})=>{
@@ -13,13 +15,31 @@ const ProductsDiv = () => {
     })
     },[])
 
+    const pages = Math.ceil(dataProducts["products"]?.length/itensPerPage)
+    const startIndex = currentPage * itensPerPage
+    const endIndex = startIndex + itensPerPage
+    const currentItens = dataProducts["products"]?.slice(startIndex, endIndex)
+    
+    
+    const arrayNumbers = []
+    for (let index = 0; index < pages; index++) {
+        arrayNumbers.push(index)   
+    }
+    
 
     return (
+        <>
         <div className="containerProducts">
-           {dataProducts["products"]?.map((item) => (
+           {currentItens?.map((item) => (
                 <div className="cards"><ProductCard title={item["title"]} description={item["description"]}/></div>
             ))}
         </div>
+        <div className="containerPagination">
+            {arrayNumbers.map((i, index) =>{
+                return <button className="pagination" value={index} onClick={(e) => setCurrentPage(e.target.value)}>{index+1}</button>
+            })}
+        </div>
+        </>
     )
 }
 
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React from "react";
 import MenuBar from "../../components/atoms/menu-bar/index"
 import ProductsDiv from "../../components/molecules/products-div/index"
 
