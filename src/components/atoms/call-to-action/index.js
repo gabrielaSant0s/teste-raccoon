74a619f657db99d2d5d89be9410c35b5ef2bbafb
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-08-11 02:34:44 -0300

    pagina de home

---
+++ src/components/atoms/call-to-action/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.css"
+
+const CallToAction = () =>{
+    return (
+        <div className="principalContainer">
+            <h1 className="titleCallToAction">Lorem ipsum</h1>
+            <p className="descriptionCallToAction">Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
+            <a href='/login' className="buttonCallToAction">Lorem Ipsum</a>
+        </div>
+    )
+}
+
+export default CallToAction
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import CallToAction from "../../components/atoms/call-to-action";
 import MenuBar from "../../components/atoms/menu-bar/index"
 
+import "./style.css"
 
 const Home = () => {
     return(
-        <div>
+        <div className="backgroundHome"> 
             <MenuBar/>
-            <h1>Página Home</h1>
+            <CallToAction/>
         </div>
     )
 }
